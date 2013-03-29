887a446801007e0412e0a0fa8344b99372c88265
Mikkel Olrik <molrik@molrik.dk>
2013-03-29 13:51:05 +0100

    BPL slimbox styling
    Changed colours, borders and next/prev/close buttons. Animation speed
    reduced to 100 ms.

--- includes/slimbox2.js
+++ includes/slimbox2.js
@@ -8,7 +8,13 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
 	jQuery(function($) {
-		$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
+		$("a[rel^='lightbox']").slimbox({
+				/* Put custom options here */
+				captionAnimationDuration: 100,
+				imageFadeDuration: 100,
+				resizeDuration: 100,
+				overlayFadeDuration: 100
+			}, null, function(el) {
 			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 		});
 	});
